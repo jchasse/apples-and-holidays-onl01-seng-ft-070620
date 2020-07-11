cff319139677805ca6f134e443d204bd032c70c6
Jonathan Chasse <jonathan.r.chasse@gmail.com>
2020-07-11 19:39:12 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -74,7 +74,15 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  holiday_hash.each |season, data|
-  
+  holiday_hash.each do |season, data|
+    data.each do |holiday, supplies|
+      if supplies == "BBQ"
+        return holiday
+      end
+    end
+  end
+end
+
+
 
 end
